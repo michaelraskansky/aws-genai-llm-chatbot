b00bedf3b846779bde759c6ae54db069063874f4
Michael Raskansky <michrask@amazon.com>
2024-12-28 20:45:40 +0200

    fix nova video modality

--- lib/model-interfaces/idefics/functions/request-handler/adapters/nova.py
+++ lib/model-interfaces/idefics/functions/request-handler/adapters/nova.py
@@ -28,9 +28,6 @@ class Nova(MultiModalModelBase):
 
                 for message_file in message_files:
                     use_s3_path = False
-                    if message_file["type"] == Modality.VIDEO.value:
-                        use_s3_path = True
-
                     user_msg["content"].append(
                         self.get_file_message(message_file, use_s3_path=use_s3_path)
                     )
@@ -53,8 +50,6 @@ class Nova(MultiModalModelBase):
         prompts.append(user_msg)
         for file in files:
             use_s3_path = False
-            if file["type"] == Modality.VIDEO.value:
-                use_s3_path = True
 
             user_msg["content"].append(
                 self.get_file_message(file, use_s3_path=use_s3_path)
@@ -76,79 +71,7 @@ class Nova(MultiModalModelBase):
         return self._run(input, model_kwargs, files)
 
     def _run(self, input: dict, model_kwargs: dict, files: Optional[list] = None):
-        # TODO:video understanding not yet supported by converse API
-        # check if any of input["message"][N]["content"][N] has a video
-        # can be removed once video understanding is supported by converse API
-        logger.info(
-            "Checking for video in messages in history", messages=input["messages"]
-        )
-        for message in input["messages"]:
-            for content in message["content"]:
-                if Modality.VIDEO.value.lower() in content:
-                    logger.info("Found video in messages, invoking native")
-                    return self._invoke_native(input, model_kwargs, files)
-
-        logger.info("No video found in messages history, invoking converse")
         return self.converse(input, model_kwargs)
-
-    def _invoke_native(
-        self, input: dict, model_kwargs: dict, files: Optional[list] = None
-    ):
-        logger.info("Incoming request for nova", model_kwargs=model_kwargs)
-        logger.info("Mode", mode=self.mode)
-        streaming = model_kwargs.get("streaming", False)
-
-        complete_response = ""
-        inf_params = {}
-
-        if "temperature" in model_kwargs:
-            inf_params["temperature"] = model_kwargs["temperature"]
-        if "topP" in model_kwargs:
-            inf_params["top_p"] = model_kwargs["topP"]
-        if "maxTokens" in model_kwargs:
-            inf_params["max_new_tokens"] = model_kwargs["maxTokens"]
-
-        native_request = {
-            "schemaVersion": "messages-v1",
-            "messages": input["messages"],
-            "inferenceConfig": inf_params,
-        }
-        logger.info("Native request", native_request=native_request)
-
-        if streaming:
-            logger.info("Calling invoke_model_with_response_stream")
-            response = self.client.invoke_model_with_response_stream(
-                modelId=self.model_id, body=json.dumps(native_request)
-            )
-            request_id = response.get("ResponseMetadata").get("RequestId")
-            logger.info(f"Request ID: {request_id}")
-            logger.info("Awaiting first token...")
-            # Process the response stream
-            stream = response.get("body")
-            if stream:
-                for event in stream:
-                    chunk = event.get("chunk")
-                    if chunk:
-                        chunk_json = json.loads(chunk.get("bytes").decode())
-                        content_block_delta = chunk_json.get("contentBlockDelta")
-                        if content_block_delta:
-                            chunk_text = content_block_delta.get("delta").get("text")
-                            self.on_llm_new_token(chunk_text)
-                            complete_response += chunk_text
-
-            return {
-                "content": complete_response,
-            }
-
-        logger.info("Calling invoke_model")
-        response = self.client.invoke_model(
-            modelId=self.model_id, body=json.dumps(native_request)
-        )
-        model_response = json.loads(response["body"].read())
-        content_text = model_response["output"]["message"]["content"][0]["text"]
-        return {
-            "content": content_text,
-        }
 
     def generate_image(
         self, input: dict, model_kwargs: dict, files: Optional[list] = None
